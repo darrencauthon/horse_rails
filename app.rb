2bc63a8525cda0402811cb58dd4b79781c60b5b1
Darren Cauthon <darren@cauthon.com>
2014-01-21 21:50:26 -0600

    Read pending tweets from the github repo.

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'horse'
+require 'httparty'
 
-options = {
-            consumer_key:        ENV['CONSUMER_KEY'],
-            consumer_secret:     ENV['CONSUMER_SECRET'],
-            access_token:        ENV['ACCESS_TOKEN'],
-            access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
-            twitter_username:    ENV['TWITTER_USERNAME'],
-            pending_tweets:      -> { ['you use mysql lol'] }
-          }
-Horse.setup(options)
+url = 'https://raw.github.com/darrencauthon/horse_rails/master/upcoming_tweets.txt'
+
+pending_tweets = HTTParty.get(url)
+                         .body
+                         .split("\n")
+                         .map    { |x| x.to_s.strip }
+                         .select { |x| x != '' }
+
+Horse.setup( {
+               consumer_key:        ENV['CONSUMER_KEY'],
+               consumer_secret:     ENV['CONSUMER_SECRET'],
+               access_token:        ENV['ACCESS_TOKEN'],
+               access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
+               twitter_username:    ENV['TWITTER_USERNAME'],
+               pending_tweets:      -> { pending_tweets }
+             } )
 
 Horse.tweet_something_new
