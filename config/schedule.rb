b33b52ee5265a35bb8a8ff2f22e67bb81755d71d
Darren Cauthon <darren@cauthon.com>
2014-02-02 12:27:07 -0600

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,10 @@
 set :job_template, ". $HOME/.profile; PATH=\"/usr/local/bin:/usr/bin:/bin\" /bin/bash -l -i -c ':job'"
 set :output, { :standard => "cron.log", :error => "cron.errors.log" }
 
-every :weekday, :at => '12pm' do
+every :weekday, :at => '1pm' do
+  rake "tweet_something"
+end
+
+every :weekday, :at => '3pm' do
   rake "tweet_something"
 end
